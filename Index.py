d3b3e76bf84eccbbfeb374b9d450388e14f50c16
RafayRao <rafayxrao@gmail.com>
2026-05-29 22:46:41 +0500

    Create a file and change in readme

---
+++ Index.py
@@ -0,0 +1,6 @@
+name = "Rafay Rao"
+print(name)
+print("Rafay Rao")
+
+# I know i Print The same Name 2 times and This is just For Learning 
+# git and github later i will add some usefull code and things
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Just Coding
 <br>
 Now Im Going to Commit
+(This is the mod in vs then i will add and commit learning git/github)
